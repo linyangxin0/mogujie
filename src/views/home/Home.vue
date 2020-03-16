829a0967d85838c7d02085b647101050af29288f
linyangxin <1227143051@qq.com>
2020-03-16 13:11:25 +0800

    better-scroll封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,63 +1,13 @@
 <template>
   <div class="home-content">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommends="recommends"></recommend-view>
-    <feature-view/>
-    <tab-control :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
-    <goods-list :goods="showGoods"></goods-list>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-      <li>9</li>
-      <li>10</li>
-      <li>11</li>
-      <li>12</li>
-      <li>13</li>
-      <li>14</li>
-      <li>15</li>
-      <li>16</li>
-      <li>17</li>
-      <li>18</li>
-      <li>19</li>
-      <li>20</li>
-      <li>21</li>
-      <li>22</li>
-      <li>23</li>
-      <li>24</li>
-      <li>25</li>
-      <li>26</li>
-      <li>27</li>
-      <li>28</li>
-      <li>29</li>
-      <li>30</li>
-      <li>31</li>
-      <li>32</li>
-      <li>33</li>
-      <li>34</li>
-      <li>35</li>
-      <li>36</li>
-      <li>37</li>
-      <li>38</li>
-      <li>39</li>
-      <li>40</li>
-      <li>41</li>
-      <li>42</li>
-      <li>43</li>
-      <li>44</li>
-      <li>45</li>
-      <li>46</li>
-      <li>47</li>
-      <li>48</li>
-      <li>49</li>
-      <li>50</li>
-    </ul>
+    <scroll class="content">
+      <home-swiper :banners="banners"></home-swiper>
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view/>
+      <tab-control :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
   </div>
 </template>
 
@@ -70,6 +20,7 @@
   import GoodsList from "components/content/GooList/GoodsList";
 
   import {getHomeMultidata,getHomeGoods} from "../../network/home";
+  import Scroll from "components/common/scroll/Scroll";
 
   export default {
     name: "Home",
@@ -79,7 +30,8 @@
       RecommendView,
       HomeSwiper,
       NavBar,
-      FeatureView
+      FeatureView,
+      Scroll
     },
     data(){
       return{
@@ -142,11 +94,23 @@
 <style scoped>
 
   .home-content{
-
+    height: 100vh;
+    position: relative;
+    background-color: white;
   }
 
   .home-nav{
     background-color: var(--color-tint);
     color: white;
+  }
+
+  .content{
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    right: 0px;
+    left: 0px;
+
+    overflow: hidden;
   }
 </style>
