d0ad1a3ff0730857e6a6b5b0c356760d165b66dd
linyangxin <1227143051@qq.com>
2020-03-19 21:10:47 +0800

    完成profile部分

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
+  <div class="cart">
     <cart-nav-bar class="nav-bar"/>
+    <scroll :probe-type="3" class="scroll">
+      <cart-goods :goodsList="goodsList"/>
+    </scroll>
     <cart-bottom-bar/>
-    <cart-goods :goodsList="goodsList"/>
   </div>
 </template>
 
@@ -10,11 +12,13 @@
   import CartNavBar from "./childComponents/CartNavBar";
   import CartBottomBar from "./childComponents/CartBottomBar";
   import CartGoods from "./childComponents/CartGoods";
+  import Scroll from "../../components/common/scroll/Scroll";
 
 
   export default {
     name: "cart",
     components:{
+      Scroll,
       CartGoods,
       CartNavBar,
       CartBottomBar
@@ -31,10 +35,18 @@
 </script>
 
 <style scoped>
+  .cart{
+    height: 100vh;
+  }
 
   .nav-bar{
     background-color: var(--color-tint);
     color: white;
     font-weight: 700;
+  }
+
+  .scroll{
+    height: calc(100% - 44px - 89px);
+    overflow: hidden;
   }
 </style>
---
+++ src/views/profile/childComponents/ProfileNavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav-bar class="nav-bar">
+    <div slot="center">
+      骚猪春商城
+    </div>
+  </nav-bar>
+</template>
+
+<script>
+
+
+  import NavBar from "components/common/navbar/NavBar";
+  export default {
+    name: "ProfileNavBar",
+    components: {NavBar}
+  }
+</script>
+
+<style scoped>
+
+  .nav-bar{
+    background-color: var(--color-tint);
+    color: #fff;
+    font-weight: 700;
+  }
+</style>
