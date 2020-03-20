a3f1147b51f7bf3c0e253b7515418246bc55e1ce
linyangxin <1227143051@qq.com>
2020-03-20 17:29:32 +0800

    修改一些基本配置

--- src/main.js
+++ src/main.js
@@ -2,14 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
 import store from "./store";
+import VueLazyLoad from 'vue-lazyload'
+import FastClick from 'fastclick'
 
 Vue.config.productionTip = false
-
-//事件总线
-Vue.prototype.$bus=new Vue()
 
 new Vue({
   render: h => h(App),
   router,
   store
 }).$mount('#app')
+
+
+Vue.use(VueLazyLoad, {
+  // preLoad: 1,
+  loading: require('./assets/img/common/placeholder.png')
+})
+
+//事件总线
+Vue.prototype.$bus=new Vue()
+
+FastClick.attach(document.body)
